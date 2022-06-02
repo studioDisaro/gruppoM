c366032b6eaac93e4b6f2de53b5f9a8d44aad217
studioDisaro <rorine@gmail.com>
2022-06-02 11:49:19 +0200

    user functions

--- functions.php
+++ functions.php
@@ -19,6 +19,24 @@
         return $records;
     }
 
+    function new_user($user_surname, $user_name, $user_username, $user_password){
+        $conn = openConDB();
+
+        $user_surname = $conn->escape_string($user_surname);
+        $user_name = $conn->escape_string($user_name);
+        $user_username = $conn->escape_string($user_username);
+        $user_password = $conn->escape_string(password_hash($user_password, PASSWORD_DEFAULT));
+
+        $sql = "INSERT `users` (`user_surname`, `user_name`, `user_username`, `user_password`) VALUES ('$user_surname', '$user_name', '$user_username', '$user_password');";
+        $res = $conn->query($sql);
+        if($res){
+            $user_id = $conn->insert_id;
+            return $user_id;
+        }else {
+            return $conn->error;
+        }
+    }
+
     function get_user_byID($user_id){
         $conn = openConDB();
         $sql = "SELECT * FROM `users` WHERE `user_id`='$user_id';";
@@ -26,6 +44,21 @@
         if($res){
             $record = $res->fetch_assoc();
             return $record;
+        }else {
+            return $conn->error;
+        }
+    }
+
+    function edit_user($user_id, $user_surname, $user_name){
+        $conn = openConDB();
+
+        $user_surname = $conn->escape_string($user_surname);
+        $user_name = $conn->escape_string($user_name);
+
+        $sql = "UPDATE `users` SET `user_surname` = '$user_surname', `user_name` = '$user_name' WHERE `user_id`='$user_id';";
+        $res = $conn->query($sql);
+        if($res){
+            return 1;
         }else {
             return $conn->error;
         }
--- users.php
+++ users.php
@@ -5,26 +5,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gestione delle utenze</title>
     <?php include_once('functions.php'); ?>
-    <?php $users = get_users_list(); ?>
+
 
     <?php
-    if (isset($_GET['user_id'])) {
-        $user = get_user_byID($_GET['user_id']);
-    }
+        if (isset($_GET['action'])) {
+            if ($_GET['action'] == "edit") {
+                edit_user($_GET['user_id'], $_GET['surname'], $_GET['name']);
+            }
+
+            if ($_GET['action'] == "new") {
+                new_user($_GET['surname'], $_GET['name'], $_GET['username'], $_GET['password']);
+            }
+        }
+
+        if (isset($_GET['user_id'])) {
+        $user_selected = get_user_byID($_GET['user_id']);
+        }
+
+        $users = get_users_list(); 
     ?>
 </head>
 
 <body>
     <ul>
         <?php foreach ($users as $user) : ?>
-            <button onclick="location='users.php?user_id=<?= $user['user_id'] ?>'"><?= $user['user_name'] ?></button>
+            <button onclick="location='users.php?user_id=<?= $user['user_id'] ?>'"><?= $user['user_surname'] ?></button>
         <?php endforeach; ?>
+
+        <button onclick="location='users.php?action=new_user'">NUOVO UTENTE</button>
+
     </ul>
 
-    <?php if (isset($user)) : ?>
-        <?php var_dump($user); ?>
+    <?php if (isset($user_selected)) : ?>
+        <?php //var_dump($user_selected); 
+        ?>
+
+        <form action="users.php" method="get">
+            <input type="hidden" name="action" value="edit">
+            <input type="hidden" name="user_id" value="<?= $user_selected['user_id'] ?>">
+            <input type="text" name="surname" value="<?= $user_selected['user_surname'] ?>">
+            <input type="text" name="name" value="<?= $user_selected['user_name'] ?>">
+            <button type="submit">Salva modifiche</button>
+        </form>
+    <?php endif; ?>
+
+    <?php if (isset($_GET['action'])) : ?>
+        <?php if ($_GET['action']=='new_user') : ?>
+            <form action="users.php" method="get">
+                <input type="hidden" name="action" value="new">
+                <input type="text" name="surname" placeholder="NOME" value="">
+                <input type="text" name="name" placeholder="COGNOME" value="">
+                <input type="text" name="username" placeholder="USERNAME" value="">
+                <input type="password" name="password" placeholder="PASSWORD" value="">
+                <button type="submit">Salva modifiche</button>
+            </form>
+        <?php endif; ?>
     <?php endif; ?>
 </body>
 
